265c35886be389201e2a08829fbaa1399399661c
Reggie <reggiebritten@gmail.com>
2020-09-10 12:41:22 -0700

    jekyll

--- assets/js/FoL.js
+++ assets/js/FoL.js
@@ -1,19 +1,14 @@
-
-
-
-
 $(document).ready(function(){
 
-
-
 //Get item and skill data
-  var dbData = [];
-  var skillData = [];
-  var itemData = [];
   $.get("items.db", function(data){
+    var dbData = [];
+    var skillData = [];
+    var itemData = [];
     data = "["+data.replace(/}\n/g, "},");
     data = data.substring(0, data.length - 1)  + "]";
     dbData = JSON.parse(data);
+
     //Do stuff with item and skill data
     for (var i = 0; i < dbData.length; i++) {
       if (dbData[i].type == "ability"){
@@ -22,8 +17,29 @@ $(document).ready(function(){
         itemData.push(dbData[i]);
       }
     }
-    console.log(skillData);
+    var threat5 = [];
+
+    for (var i = 0; i < skillData.length; i++) {
+      if (skillData[i].data.threat == 5){
+        threat5.push(skillData[i]);
+      }
+    }
+
+    for (var i = 0; i < threat5.length; i++) {
+      //console.log(threat5[i].data.summary)
+
+
+      //document.getElementById("skills").innerHTML += threat5[i].data.summary;
+
+    }
+
+
+
+
+
   });
+
+
 
 
 
@@ -40,29 +56,11 @@ $(document).ready(function(){
   });
 
 
-
- // var pattern = /\b(of|the|in|and)/gi; // target whole words globally and case insensitive
-
-
-
-
- /* //Convert keywords into links
-  const keywords = ["Bleeding", "Blessed", "Blinded", "Burning", "Chilled", "Choking", "Compelled", "Corroding", "Cursed", "Deafened", "Defenseless", "Distracted", "Drunk", "Empowered", "Encumbered", "Frightened", "Fumbling", "Grabbed", "Guarded", "Hastened", "Hidden", "Honed", "Invisible", "Kneeling", "Oiled", "Petrified", "Provoked", "Relaxed", "Restrained", "Shocked", "Slowed", "Staggered", "Stunned", "Surprised", "Unbalanced", "Unconscious", "Vortexed", "Weakened", "Wet", "Wracked", "Disarmed", "Knocked-Down", "Confused", "bleeding", "blessed", "blinded", "burning", "chilled", "choking", "compelled", "corroding", "cursed", "deafened", "defenseless", "distracted", "drunk", "empowered", "encumbered", "frightened", "fumbling", "grabbed", "guarded", "hastened", "hidden", "honed", "invisible", "kneeling", "oiled", "petrified", "provoked", "relaxed", "restrained", "shocked", "slowed", "staggered", "stunned", "surprised", "unbalanced", "unconscious", "vortexed", "weakened", "wet", "wracked", "disarmed", "knocked-down", "confused", "base focus", "base momentum", "base poise","Base focus", "Base momentum", "Base poise"];
-  keywords.forEach(function(keyword){
-     var pattern = new RegExp('\\b('+ keyword+ ')', 'gi');
-     var replaceWith = '<span class="keyword">$1</span>';
-     $('p').each(function(){
-       $(this).html($(this).html().replace(pattern,replaceWith));
-     });
-
-   });
-  */
-
 //Convert keywords into links
-   const keywords = ["Bleeding", "Blessed", "Blinded", "Burning", "Chilled", "Choking", "Compelled", "Corroding", "Cursed", "Deafened", "Defenseless", "Distracted", "Drunk", "Empowered", "Encumbered", "Frightened", "Fumbling", "Grabbed", "Guarded", "Hastened", "Hidden", "Honed", "Invisible", "Kneeling", "Oiled", "Petrified", "Provoked", "Relaxed", "Restrained", "Shocked", "Slowed", "Staggered", "Stunned", "Surprised", "Unbalanced", "Unconscious", "Vortexed", "Weakened", "Wet", "Wracked", "Disarmed", "Knocked-Down", "Confused", "Base focus", "Base momentum", "Base poise"];
+   const keywords = ["Bleeding", "Blessed", "Blinded", "Burning", "Chilled", "Choking", "Compelled", "Corroding", "Cursed", "Deafened", "Defenseless", "Distracted", "Drunk", "Empowered", "Encumbered", "Frightened", "Fumbling", "Grabbed", "Guarded", "Hastened", "Hidden", "Honed", "Invisible", "Kneeling", "Oiled", "Petrified", "Provoked", "Relaxed", "Restrained", "Shocked", "Slowed", "Staggered", "Stunned", "Surprised", "Unbalanced", "Unconscious", "Vortexed", "Weakened", "Wet", "Wracked", "Disarmed", "Knocked-Down", "Confused", "Base focus", "Base momentum", "Base poise", "Attack", "Boon", "Boons"];
     keywords.forEach(function(keyword){
       var re = new RegExp("("+keyword+")", 'gi');
-      Array.from(document.getElementsByClassName("FoLedit")).forEach(
+      Array.from(document.getElementsByTagName("P")).forEach(
         element => element.innerHTML = element.innerHTML.replace(re, `<a href="#${keyword.toLowerCase()}" title="tooltipPlaceHolder" class="keyword-${keyword.replace(' ', '-').toLowerCase()}"><strong>$1</strong></a>`)
         );
     });
@@ -71,8 +69,8 @@ $(document).ready(function(){
 //Add icons before relevant text
    const icons = ["Bleeding", "Blessed", "Blinded", "Burning", "Chilled", "Choking", "Compelled", "Corroding", "Cursed", "Deafened", "Defenseless", "Distracted", "Drunk", "Empowered", "Encumbered", "Frightened", "Fumbling", "Grabbed", "Guarded", "Hastened", "Hidden", "Honed", "Invisible", "Kneeling", "Oiled", "Petrified", "Provoked", "Relaxed", "Restrained", "Shocked", "Slowed", "Staggered", "Stunned", "Surprised", "Unbalanced", "Unconscious", "Vortexed", "Weakened", "Wet", "Wracked", "Focus", "Momentum", "Poise", "Disarmed", "Knocked-Down", "Confused"];
    icons.forEach(function(icon){
-      var re = new RegExp(`(?<!base\\s)(?<!Base\\s)(${icon.toLowerCase()})(?!"><strong>)`, 'gi');
-      Array.from(document.getElementsByClassName("FoLedit")).forEach(
+      var re = new RegExp(`(?<!base\\s)(?<!Base\\s)(?<!#)(${icon.toLowerCase()})(?!"><strong>)`, 'gi');
+      Array.from(document.getElementsByTagName("P")).forEach(
         element => element.innerHTML = element.innerHTML.replace(re, `<img src='/images/icons/${icon.toLowerCase()}.png' class='inlineicon'> <strong>$1</strong>`)
         );
     });
@@ -80,7 +78,7 @@ $(document).ready(function(){
 
 //Bold numbers
     var re= new RegExp('(^|\\s)((\\+|\\-)\\d+)(\\s|$)', 'g');
-    Array.from(document.getElementsByClassName("FoLedit")).forEach(element => element.innerHTML = element.innerHTML.replace(re, '<strong>$1$2$4</strong>'));
+    Array.from(document.getElementsByTagName("P")).forEach(element => element.innerHTML = element.innerHTML.replace(re, '<strong>$1$2$4</strong>'));
 
 
 //Get journal data
@@ -95,7 +93,7 @@ $(document).ready(function(){
         for (var i=0; i < journalData.length; i++) {
           if (journalData[i].name.toLowerCase() === keyword.toLowerCase()) {
             $('.keyword-'+keyword.replace(' ', '-').toLowerCase()).tooltip({
-            content: `<h3>${keyword}</h3>${journalData[i].content}`,
+            content: `<div class="box tooltip"><img src="images/icons/${keyword}.png" class="tooltip-icon"><h2>${keyword}</h2>${journalData[i].content}</div>`,
             position: { my: "left top+15", at: "left bottom", collision: "flipfit" },
             });
           }
@@ -106,6 +104,13 @@ $(document).ready(function(){
 
 
   
+
+
+
+
+
+
+
 
 
 
@@ -132,3 +137,12 @@ function updateHash() {
 window.onhashchange = updateHash;
 
 $(".ui-helper-hidden-accessible").remove();
+
+
+
+
+
+
+
+
+
