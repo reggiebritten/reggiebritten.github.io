dc11691ec5f2b1105b91ab1ba4ab95da691fed6f
Reggie <reggiebritten@gmail.com>
2020-09-25 15:36:20 -0700

    Base Pages Added

--- assets/js/FoL.js
+++ assets/js/FoL.js
@@ -56,31 +56,125 @@ $(document).ready(function(){
   });
 
 
-//Convert keywords into links
-   const keywords = ["Bleeding", "Blessed", "Blinded", "Burning", "Chilled", "Choking", "Compelled", "Corroding", "Cursed", "Deafened", "Defenseless", "Distracted", "Drunk", "Empowered", "Encumbered", "Frightened", "Fumbling", "Grabbed", "Guarded", "Hastened", "Hidden", "Honed", "Invisible", "Kneeling", "Oiled", "Petrified", "Provoked", "Relaxed", "Restrained", "Shocked", "Slowed", "Staggered", "Stunned", "Surprised", "Unbalanced", "Unconscious", "Vortexed", "Weakened", "Wet", "Wracked", "Disarmed", "Knocked-Down", "Confused", "Base focus", "Base momentum", "Base poise", "Attack", "Boon"];
-    keywords.forEach(function(keyword){
-      var re = new RegExp("(?<!&nbsp;)("+keyword+"s?)", 'gi');
-      Array.from(document.getElementsByTagName("P")).forEach(
-        element => element.innerHTML = element.innerHTML.replace(re, `<a href="#${keyword.toLowerCase()}" title="tooltipPlaceHolder" class="keyword-${keyword.replace(' ', '-').toLowerCase()}"><strong>$1</strong></a>`)
-        );
-    });
 
 
-//Add icons before relevant text
-   const icons = ["Bleeding", "Blessed", "Blinded", "Burning", "Chilled", "Choking", "Compelled", "Corroding", "Cursed", "Deafened", "Defenseless", "Distracted", "Drunk", "Empowered", "Encumbered", "Frightened", "Fumbling", "Grabbed", "Guarded", "Hastened", "Hidden", "Honed", "Invisible", "Kneeling", "Oiled", "Petrified", "Provoked", "Relaxed", "Restrained", "Shocked", "Slowed", "Staggered", "Stunned", "Surprised", "Unbalanced", "Unconscious", "Vortexed", "Weakened", "Wet", "Wracked", "Focus", "Momentum", "Poise", "Disarmed", "Knocked-Down", "Confused"];
-   icons.forEach(function(icon){
-      var re = new RegExp(`(?<!base\\s)(?<!Base\\s)(?<!#)(?<!&nbsp;)(${icon.toLowerCase()}s?)(?!"><strong>)`, 'gi');
-      Array.from(document.getElementsByTagName("P")).forEach(
-        element => element.innerHTML = element.innerHTML.replace(re, `<img src='/images/icons/${icon.toLowerCase()}.png' class='inlineicon'> <strong>$1</strong>`)
-        );
+//Create keyword links, icons & tooltips
+  $.get("/assets/data/keywords.json", function(json){
+    json.forEach(function(keyword){
+
+//Define Variables
+      var re = new RegExp('^([^]*?)(?<!&nbsp;|/|="|base )(\\b'+keyword.keyword+'s?\\b)(?! dice| die| pool| terrain)', 'gi');
+
+      var link = '';
+      var closelink = '';
+      var tooltipClass = '';
+      var icon = '';
+      if (keyword.icon) {
+        icon= `<img src='/images/icons/${keyword.keyword}.png'>`
+      };
+
+
+
+    //Create Tooltips
+    if (keyword.page){
+      $.get(`/${keyword.page}.html`, function(data){
+        var paragraphs = $(`#${keyword.hash}`, $(data)).nextUntil('h2, h1, h3', ':not(blockquote)');
+        var test = $(`#${keyword.hash}`, $(data)).nextUntil('h2, h1, h3', ':not(blockquote)');
+        var content= '';
+
+        paragraphs.each(function(stuff){
+        // console.log($(stuff).wrap( "<p></p>" ))
+        })
+        paragraphs.html(function (i, html){
+        
+          //console.log(html)
+        
+        
+          content += html;
+
+
+
+
+
+
+
+
+
+          //tooltip images
+
+          json.forEach(function(keywordtooltip){
+            var re2 = new RegExp('^([^]*?)(?<!&nbsp;|/|="|base )(\\b'+keywordtooltip.keyword+'s?\\b)(?! dice| die| pool)', 'gi');
+          if (keywordtooltip.icon) {
+            img2= `<img src='/images/icons/${keywordtooltip.keyword}.png'>`;
+          }
+          content= content.replace(re2, `$1<strong>${img2}&nbsp;$2</strong>`);
+        })
+
+
+
+
+
+
+
+
+          $(`.keyword-${keyword.keyword.replace(' ', '-')}`).tooltip({
+            content: `<blockquote>${icon}<h2>${keyword.keyword}</h2>${content}</blockquote>`,
+            position: { my: "left top+15", at: "left bottom", collision: "flipfit" },
+            });
+        })
     });
+    }
+
+
+
+
+
+
+
+
+//Create links      
+      $('p').html(function (i, html){
+        if (keyword.page) {
+          tooltipClass = `keyword-${keyword.keyword.replace(' ', '-')}`;
+          link = `<a href="/${keyword.page}.html#${keyword.hash}" title="tooltipPlaceHolder" class="${tooltipClass}">`;
+          closelink = `</a>`;
+        }
+        return html.replace(re, `$1${link}<strong>${icon} $2</strong>${closelink}`)
+      }
+      );
+    
+    
+    
+
+    
+    
+    
+
+    
+    
+
+
+
+
+
+    
+    });
+  });
+
+
+
+
 
 
 //Bold numbers
     var re= new RegExp('(^|\\s)((\\+|\\-)\\d+)(\\s|$)', 'g');
-    Array.from(document.getElementsByTagName("P")).forEach(element => element.innerHTML = element.innerHTML.replace(re, '<strong>$1$2$4</strong>'));
+    $('p').html(function (i, html){
+        return html.replace(re, '<strong>$1$2$4</strong>');
+    });
 
+    
 
+/*
 //Get journal data
   $.get("/assets/data/journal.db", function(data){
     let journalData = [];
@@ -89,11 +183,12 @@ $(document).ready(function(){
     journalData = JSON.parse(data);
 
 //Add tooltips
+const keywords = ["bleeding", "blessed", "blinded", "burning", "chilled", "choking", "compelled", "corroding", "cursed", "deafened", "defenseless", "distracted", "drunk", "empowered", "encumbered", "frightened", "fumbling", "grabbed", "guarded", "hastened", "hidden", "honed", "invisible", "kneeling", "oiled", "petrified", "provoked", "relaxed", "restrained", "shocked", "slowed", "staggered", "stunned", "surprised", "unbalanced", "unconscious", "vortexed", "weakened", "wet", "wracked", "disarmed", "knocked-down", "confused", "base focus", "base momentum", "base poise", "attack", "boon", "bane", "legendary deed", "fateful deed", "fate", "fate dice", "fate die", "doom", "skill", "personality", "personal legend", "fated effect", "condition", "special terrain", "fighting style", "stress", "affliction"];
       keywords.forEach(function(keyword){
         for (var i=0; i < journalData.length; i++) {
           if (journalData[i].name.toLowerCase() === keyword.toLowerCase()) {
             $('.keyword-'+keyword.replace(' ', '-').toLowerCase()).tooltip({
-            content: `<div class="box tooltip"><img src="images/icons/${keyword}.png" class="tooltip-icon"><h2>${keyword}</h2>${journalData[i].content}</div>`,
+            content: `<blockquote><img src="images/icons/${keyword}.png" class="tooltip-icon"><h2>${keyword}</h2>${journalData[i].content}</blockquote>`,
             position: { my: "left top+15", at: "left bottom", collision: "flipfit" },
             });
           }
@@ -101,17 +196,36 @@ $(document).ready(function(){
       });
   });
 
-
+*/
 
   
 
 
 
 
+//$( "section h1" ).wrapInner(function() {
+// return "<span class='gold' data-heading='" + this.innerHTML + "'></span>";
+// });
+  
+//  $( "section h2" ).wrapInner(function() {
+//    return "<span class='silver' data-heading='" + this.innerHTML + "'></span>";
+// });
+
+  
+//$('section p:first-of-type').html(function (i, html)
+//{
+//   return html.replace(/^[^a-zA-Z]*([a-zA-Z])/g, '<span class="gold dropcap" data-heading="$1">$1</span>');
+//});
 
 
 
 
+
+
+
+
+
+$('blockquote p:first-of-type img').addClass( "quoteimg" );
 
 
 
@@ -136,8 +250,18 @@ function updateHash() {
 
 window.onhashchange = updateHash;
 
-$(".ui-helper-hidden-accessible").remove();
 
+
+
+
+
+
+
+
+
+
+
+//$(".ui-helper-hidden-accessible").remove();
 
 
 
